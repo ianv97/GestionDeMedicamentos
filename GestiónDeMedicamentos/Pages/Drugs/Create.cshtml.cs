90579366011f9037559ccc12b85abee3880b2d18
Ian Vaernet <ianalexisnet@hotmail.com>
2019-05-18 19:24:07 -0300

    Listado de drogas actualizado

---
+++ GestiónDeMedicamentos/Pages/Drugs/Create.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using GestiónDeMedicamentos.Database;
+using GestiónDeMedicamentos.Models;
+
+namespace GestiónDeMedicamentos.Pages.Drugs
+{
+    public class CreateModel : PageModel
+    {
+        private readonly GestiónDeMedicamentos.Database.PostgreContext _context;
+
+        public CreateModel(GestiónDeMedicamentos.Database.PostgreContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Drug Drug { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Drugs.Add(Drug);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ GestiónDeMedicamentos/Pages/Drugs/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using GestiónDeMedicamentos.Database;
+using GestiónDeMedicamentos.Models;
+
+namespace GestiónDeMedicamentos.Pages.Drugs
+{
+    public class IndexModel : PageModel
+    {
+        private readonly GestiónDeMedicamentos.Database.PostgreContext _context;
+
+        public IndexModel(GestiónDeMedicamentos.Database.PostgreContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Drug> Drug { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Drug = await _context.Drugs.ToListAsync();
+        }
+    }
+}
