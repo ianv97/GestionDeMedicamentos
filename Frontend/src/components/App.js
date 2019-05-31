de5d0e9cac4e6a1f64cf9fe85a46dad06f42d7bf
Ian Vaernet <ianalexisnet@hotmail.com>
2019-05-31 01:26:58 -0300

    Navbar

--- Frontend/src/components/App.js
+++ Frontend/src/components/App.js
@@ -1,26 +1,6 @@
 import React from "react";
-import clsx from "clsx";
-import { makeStyles, useTheme } from "@material-ui/core/styles";
-import Drawer from "@material-ui/core/Drawer";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import List from "@material-ui/core/List";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import Typography from "@material-ui/core/Typography";
-import Divider from "@material-ui/core/Divider";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
-import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
-import ChevronRightIcon from "@material-ui/icons/ChevronRight";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
-import ListItemText from "@material-ui/core/ListItemText";
-import LocalHospitalIcon from "@material-ui/icons/LocalHospital";
-import DragIndicatorIcon from "@material-ui/icons/DragIndicator";
-import InputIcon from "@material-ui/icons/Input";
-import CallMissedOutgoingIcon from "@material-ui/icons/CallMissedOutgoing";
-import DoneOutlineIcon from "@material-ui/icons/DoneOutline";
-import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import Navbar from "./Navbar";
+import { Switch, Route } from "react-router-dom";
 import Inicio from "../pages/Inicio";
 import Drogas from "../pages/Drogas";
 import Medicamentos from "../pages/Medicamentos";
@@ -28,179 +8,19 @@ import Reposiciones from "../pages/Reposiciones";
 import Partidas from "../pages/Partidas";
 import Stock from "../pages/Stock";
 
-const drawerWidth = 240;
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    display: "flex"
-  },
-  appBar: {
-    zIndex: theme.zIndex.drawer + 1,
-    transition: theme.transitions.create(["width", "margin"], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen
-    })
-  },
-  appBarShift: {
-    marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(["width", "margin"], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen
-    })
-  },
-  menuButton: {
-    marginRight: 36
-  },
-  hide: {
-    display: "none"
-  },
-  drawer: {
-    width: drawerWidth,
-    flexShrink: 0,
-    whiteSpace: "nowrap"
-  },
-  drawerOpen: {
-    width: drawerWidth,
-    transition: theme.transitions.create("width", {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen
-    })
-  },
-  drawerClose: {
-    transition: theme.transitions.create("width", {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen
-    }),
-    overflowX: "hidden",
-    width: theme.spacing(7) + 1,
-    [theme.breakpoints.up("sm")]: {
-      width: theme.spacing(9) + 1
-    }
-  },
-  toolbar: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "flex-end",
-    padding: "0 8px",
-    ...theme.mixins.toolbar
-  },
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing(3)
-  }
-}));
-
-function MiniDrawer() {
-  const classes = useStyles();
-  const theme = useTheme();
-  const [open, setOpen] = React.useState(false);
-
-  function handleDrawerOpen() {
-    setOpen(true);
-  }
-
-  function handleDrawerClose() {
-    setOpen(false);
-  }
-
+function App() {
   return (
-    <div className={classes.root}>
-      <CssBaseline />
-      <AppBar
-        position="fixed"
-        className={clsx(classes.appBar, {
-          [classes.appBarShift]: open
-        })}
-      >
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="Open drawer"
-            onClick={handleDrawerOpen}
-            edge="start"
-            className={clsx(classes.menuButton, {
-              [classes.hide]: open
-            })}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" noWrap>
-            Gestión de medicamentos
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <BrowserRouter>
-        <Drawer
-          variant="permanent"
-          className={clsx(classes.drawer, {
-            [classes.drawerOpen]: open,
-            [classes.drawerClose]: !open
-          })}
-          classes={{
-            paper: clsx({
-              [classes.drawerOpen]: open,
-              [classes.drawerClose]: !open
-            })
-          }}
-          open={open}
-        >
-          <div className={classes.toolbar}>
-            <IconButton onClick={handleDrawerClose}>
-              {theme.direction === "rtl" ? (
-                <ChevronRightIcon />
-              ) : (
-                <ChevronLeftIcon />
-              )}
-            </IconButton>
-          </div>
-          <Divider />
-          <List>
-            {[
-              "Drogas",
-              "Medicamentos",
-              "Reposiciones",
-              "Partidas",
-              "Stock"
-            ].map((text, index) => (
-              <ListItem button component={Link} to={"/" + text} key={text}>
-                <ListItemIcon>
-                  <i className={"fas fa-2x fa-" + iconSwitch(index)} />
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItem>
-            ))}
-          </List>
-        </Drawer>
-        <main className={classes.content}>
-          <div className={classes.toolbar} />
-          <Switch>
-            <Route exact path="/" component={Inicio} />
-            <Route exact path="/Drogas" component={Drogas} />
-            <Route exact path="/Medicamentos" component={Medicamentos} />
-            <Route exact path="/Reposiciones" component={Reposiciones} />
-            <Route exact path="/Partidas" component={Partidas} />
-            <Route exact path="/Stock" component={Stock} />
-          </Switch>
-        </main>
-      </BrowserRouter>
-    </div>
+    <Navbar>
+      <Switch>
+        <Route exact path="/" component={Inicio} />
+        <Route exact path="/Drogas" component={Drogas} />
+        <Route exact path="/Medicamentos" component={Medicamentos} />
+        <Route exact path="/Reposiciones" component={Reposiciones} />
+        <Route exact path="/Partidas" component={Partidas} />
+        <Route exact path="/Stock" component={Stock} />
+      </Switch>
+    </Navbar>
   );
 }
 
-function iconSwitch(index) {
-  switch (index) {
-    case 0:
-      return "prescription-bottle-alt";
-    case 1:
-      return "pills";
-    case 2:
-      return "sign-in-alt";
-    case 3:
-      return "sign-out-alt";
-    case 4:
-      return "clipboard-list";
-  }
-}
-
-export default MiniDrawer;
+export default App;
